9c414832c1ec3f1364fc7a95beb64628bef2f98c
crusheric <153489736+crusheric@users.noreply.github.com>
2023-12-16 02:04:13 +0800

    Legal

---
+++ app/(legal)/privacy/page.tsx
@@ -0,0 +1,123 @@
+import Hero from "@/components/Hero";
+
+export default function Home() {
+    return (
+        <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+
+<h1 className="bold-40 lg:bold-48">CRUSH - Privacy Policy</h1>
+Privacy Policy | Last updated December 15, 2023<br/>
+
+This privacy policy applies between any third party that accesses the website that you are currently using, crush.to, and any sub-domains of this site unless expressly excluded by their own terms and conditions (“Website”) and is not either (i) employed by CRSUH, the owner and provider of this Website (“we”, “us”), and acting in the course of their employment or (ii) engaged as a consultant or otherwise providing services to CRSUH and accessing the Website in connection with the provision of such services and CRSUH (“user”, “you”). This privacy policy applies to our use of any and all Data collected by us or provided by you in relation to your use of the Website.<br/><br/>
+This privacy policy should be read alongside, and in addition to, our Terms and Conditions.<br/><br/>
+Before using the Services or submitting any Personal Information to Character, please review this Privacy Policy carefully. By using the Services, you agree to the practices described in this Privacy Policy. If you do not agree to this Privacy Policy, you may not access any of our Websites or otherwise use the Service.<br/><br/>
+
+Definitions and interpretation<br/>
+In this privacy policy, unless the context requires a different interpretation:
+the singular includes the plural and vice versa;
+references to sub-clauses, clauses, schedules or appendices are to sub-clauses, clauses, schedules or appendices of this privacy policy;
+a reference to a person includes firms, companies, government entities, trusts and partnerships;
+"including" is understood to mean "including without limitation";
+reference to any statutory provision includes any modification or amendment of it;
+the headings and sub-headings do not form part of this privacy policy.<br/><br/>
+Scope of this privacy policy<br/>
+This privacy policy applies only to the actions of CRSUH and Users with respect to this Website. It does not extend to any websites that can be accessed from this Website including, but not limited to, any links we may provide to social media websites. For purposes of any applicable law relating to the processing of personal Data, including but not limited to the the UK General Data Protection Regulation (“GDPR”), and any national implementing and supplementary laws, regulations and secondary legislation (“Data Protection Laws”), CRSUH is the "data controller". This means that CRSUH determines the purposes for which, and the manner in which, collectively all information that you submit to CRSUH via the Website. This definition incorporates, where applicable, the definitions provided in the Data Protection Laws (“Data”) is processed.
+<br/><br/>Data collected<br/>
+We may collect the following Data, which includes personal Data, from you: name; date of birth; gender; job title; profession; contact Information such as email addresses and telephone numbers; demographic information such as preferences and interests; financial information such as credit / debit card numbers; IP address (automatically collected); web browser type and version (automatically collected); operating system (automatically collected); a list of URLs starting with a referring site, your activity on this Website, and the site you exit to (automatically collected); in each case, in accordance with this privacy policy.
+For people who create their own pages on our website (“Talents”), we in addition collect data directly and from third parties. Depending on the type of profile you create, you may have to provide us with some combination of the following:<br/><br/>
+
+bank account and address information to process payouts with third parties,<br/>
+additional information for tax purposes,<br/>
+email address;<br/>
+phone number;<br/>
+username;<br/>
+password;<br/>
+first and last name/surname;<br/>
+address;<br/>
+social security number and/or employer identification number;<br/>
+country of citizenship;<br/>
+tax identification number and/or VAT number;<br/>
+date of birth;<br/>
+bank account information;<br/>
+custom content and voice samples;<br/>
+self identification as an Adult/18+ creator; and/or<br/>
+copies of your ID card and proof of residency.<br/><br/>
+
+How we collect Data<br/>
+We collect Data in the following ways: Data that is given to us by you; and Data that is collected automatically.<br/><br/>
+Data that is given to us by you<br/>
+We will collect your Data in a number of ways, for example:<br/>
+when you contact us through the Website, by telephone, post, e-mail or through any other means;<br/>
+when you register with us and set up an account to receive our products/services;<br/>
+when you register with us as a Talent and set up a chatbot account;<br/>
+when you complete surveys that we use for research purposes (although you are not obliged to respond to them);<br/>
+when you enter a competition or promotion through a social media channel;<br/>
+when you make payments to us, through this Website or otherwise;<br/>
+when you elect to receive marketing communications from us;<br/>
+when you use our services;<br/>
+in each case, in accordance with this privacy policy.<br/><br/>
+Data that is collected automatically<br/>
+To the extent that you access the Website, we will collect your Data automatically, for example we automatically collect some information about your visit to the Website. This information helps us to make improvements to Website content and navigation, and includes your IP address, the date, times and frequency with which you access the Website and the way you use and interact with its content.
+<br/><br/>Our use of Data<br/>
+Any or all of the above Data may be required by us from time to time in order to provide you with the best possible service and experience when using our Website. Specifically, Data may be used by us for the following reasons:<br/>
+internal record keeping;<br/>
+improvement of our products / services;<br/>
+transmission by email of marketing materials that may be of interest to you;<br/>
+contact for market research purposes which may be done using email, telephone, fax or mail. Such information may be used to customise or update the Website;<br/>
+in each case, in accordance with this privacy policy.<br/><br/>
+We may use your Data for the above purposes if we deem it necessary to do so for our legitimate interests. If you are not satisfied with this, you have the right to object in certain circumstances (see the section headed "Your rights" below).
+<br/><br/>For the delivery of direct marketing to you via e-mail, we'll need your consent, whether via an opt-in or soft-opt-in:<br/><br/>
+soft opt-in consent is a specific type of consent which applies when you have previously engaged with us (for example, you contact us to ask us for more details about a particular product/service, and we are marketing similar products/services). Under "soft opt-in" consent, we will take your consent as given unless you opt-out.
+<br/>for other types of e-marketing, we are required to obtain your explicit consent; that is, you need to take positive and affirmative action when consenting by, for example, checking a tick box that we'll provide.
+<br/>if you are not satisfied with our approach to marketing, you have the right to withdraw consent at any time. To find out how to withdraw your consent, see the section headed "Your rights" below.
+<br/><br/>When you register with us and set up an account to receive our services, the legal basis for this processing is the performance of a contract between you and us and/or taking steps, at your request, to enter into such a contract.
+<br/><br/>Who we share Data with<br/>
+We may share your Data with the following groups of people for the following reasons:<br/><br/>
+our employees, agents and/or professional advisors - to use it for legitimate business interests;<br/>
+third party service providers who provide services to us which require the processing of personal data - to use it for legitimate business interests;<br/>
+third party payment providers who process payments made over the Website - to enable third party payment providers to process user payments and refunds, and issue payouts to creators;<br/>
+in each case, in accordance with this privacy policy.<br/><br/>
+Keeping Data secure<br/>
+We will use technical and organisational measures to safeguard your Data, for example:<br/>
+access to your account is controlled by a password and a user name that is unique to you.<br/>
+we store your Data on secure servers.<br/>
+payment details are encrypted using SSL technology (typically you will see a lock icon or green address bar (or both) in your browser when we use this technology.<br/><br/>
+Technical and organisational measures include measures to deal with any suspected data breach.<br/>
+If you suspect any misuse or loss or unauthorised access to your Data, please let us know immediately by contacting us via this e-mail address: support@crush.to.<br/>
+If you want detailed information from Get Safe Online on how to protect your information and your computers and devices against fraud, identity theft, viruses and many other online problems, please visit www.getsafeonline.org. Get Safe Online is supported by HM Government and leading businesses.<br/>
+<br/><br/>Data retention<br/>
+Unless a longer retention period is required or permitted by law, we will only hold your Data on our systems for the period necessary to fulfil the purposes outlined in this privacy policy or until you request that the Data be deleted.
+Even if we delete your Data, it may persist on backup or archival media for legal, tax or regulatory purposes.
+<br/><br/>Your rights<br/>
+You have the following rights in relation to your Data:<br/>
+Right to access - the right to request (i) copies of the information we hold about you at any time, or (ii) that we modify, update or delete such information. If we provide you with access to the information we hold about you, we will not charge you for this, unless your request is "manifestly unfounded or excessive." Where we are legally permitted to do so, we may refuse your request. If we refuse your request, we will tell you the reasons why.
+<br/>Right to correct - the right to have your Data rectified if it is inaccurate or incomplete.
+<br/>Right to erase - the right to request that we delete or remove your Data from our systems.
+<br/>Right to restrict our use of your Data - the right to "block" us from using your Data or limit the way in which we can use it.
+<br/>Right to data portability - the right to request that we move, copy or transfer your Data.
+<br/>Right to object- the right to object to our use of your Data including where we use it for our legitimate interests.
+<br/><br/>To make enquiries, exercise any of your rights set out above, or withdraw your consent to the processing of your Data (where consent is our legal basis for processing your Data), please contact us via this e-mail address: support@crush.to
+<br/><br/>If you are not satisfied with the way a complaint you make in relation to your Data is handled by us, you may be able to refer your complaint to the relevant data protection authority. For the UK, this is the Information Commissioner's Office (ICO). The ICO's contact details can be found on their website at https://ico.org.uk/.
+<br/><br/>It is important that the Data we hold about you is accurate and current. Please keep us informed if your Data changes during the period for which we hold it.
+<br/><br/>Transfers outside the United Kingdom and European Economic Area<br/>
+Data which we collect from you may be stored and processed in and transferred to countries outside of the UK and European Economic Area (EEA). For example, this could occur if our servers are located in a country outside the UK or EEA or one of our service providers is situated in a country outside the UK or EEA.
+We will only transfer Data outside the UK or EEA where it is compliant with data protection legislation and the means of transfer provides adequate safeguards in relation to your data, eg by way of data transfer agreement, incorporating the current standard contractual clauses adopted by the European Commission.
+To ensure that your Data receives an adequate level of protection, we have put in place appropriate safeguards and procedures with the third parties we share your Data with. This ensures your Data is treated by those third parties in a way that is consistent with the Data Protection Laws.
+<br/><br/>Links to other websites<br/>
+This Website may, from time to time, provide links to other websites. We have no control over such websites and are not responsible for the content of these websites. This privacy policy does not extend to your use of such websites. You are advised to read the privacy policy or statement of other websites prior to using them.
+<br/><br/>Changes of business ownership and control<br/>
+We may, from time to time, expand or reduce our business and this may involve the sale and/or the transfer of control of all or part of CRSUH. Data provided by Users will, where it is relevant to any part of our business so transferred, be transferred along with that part and the new owner or newly controlling party will, under the terms of this privacy policy, be permitted to use the Data for the purposes for which it was originally supplied to us.
+We may also disclose Data to a prospective purchaser of our business or any part of it.
+In the above instances, we will take steps with the aim of ensuring your privacy is protected.
+<br/><br/>General<br/>
+You may not transfer any of your rights under this privacy policy to any other person. We may transfer our rights under this privacy policy where we reasonably believe your rights will not be affected.
+If any court or competent authority finds that any provision of this privacy policy (or part of any provision) is invalid, illegal or unenforceable, that provision or part-provision will, to the extent required, be deemed to be deleted, and the validity and enforceability of the other provisions of this privacy policy will not be affected.
+Unless otherwise agreed, no delay, act or omission by a party in exercising any right or remedy will be deemed a waiver of that, or any other, right or remedy.
+This Agreement will be governed by and interpreted according to the law of England and Wales. All disputes arising under the Agreement will be subject to the exclusive jurisdiction of the English and Welsh courts.
+<br/><br/>Changes to this privacy policy<br/>
+CRSUH reserves the right to change this privacy policy as we may deem necessary from time to time or as may be required by law. Any changes will be immediately posted on the Website and you are deemed to have accepted the terms of the privacy policy on your first use of the Website following the alterations.
+This privacy policy was created on 15 December 2023.
+
+
+</section>
+        )
+  }
