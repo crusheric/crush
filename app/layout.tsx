58df9b1450e994cd197754786ae2f36fb7ab6c3a
crusheric <153489736+crusheric@users.noreply.github.com>
2024-01-12 15:37:55 +0800

    GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next';
+import Script from 'next/script'
+const GTM_ID = 'GTM-NXMZ3RMM';
 
 import './globals.css'
 import Navbar from '@/components/Navbar';
@@ -16,6 +18,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+    </Script>
       <body>
         <Navbar />
         <main className="relative overflow-hidden">
