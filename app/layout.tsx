060048aa842e2cc85af96dae8ce2c7be25aadc23
crusheric <153489736+crusheric@users.noreply.github.com>
2024-01-12 15:46:07 +0800

    GTM + GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,11 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <noscript
+        dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+        }} 
+    />
       </body>
     </html>
   )
