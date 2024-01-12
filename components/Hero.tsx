0a58d1481e08b50f7eb83553b3a85511e7d0bb0b
crusheric <153489736+crusheric@users.noreply.github.com>
2024-01-12 12:57:24 +0800

    avatar block

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,71 +6,51 @@ const Hero = () => {
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
-        <h1 className="bold-52 lg:bold-88">Chat with your virtual girlfriend</h1>
-        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-        Whether it's late at night, or the start of your day, CRUSH models will be always by your side.
-        </p>
-        <video src={require('public/bg_video.mp4')} autoPlay muted loop className="styles.video" />
-
-        <div className="my-11 flex flex-wrap gap-5">
-          <div className="flex items-center gap-2">
-            {Array(5).fill(1).map((_, index) => (
-              <Image 
-                src="/star.svg"
-                key={index}
-                alt="star"
-                width={24}
-                height={24}
-              />
-            ))}
-          </div>
-
-          
-
-          <p className="bold-16 lg:bold-20 text-blue-70">
-            2.3K
-            <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
-          </p>
-        </div>
-         {/*}
-        <div className="flex flex-col w-full gap-3 sm:flex-row">
-          <Button 
-            type="button" 
-            title="Download App" 
-            variant="btn_green" 
-          />
-          <Button 
-            type="button" 
-            title="How we work?" 
-            icon="/play.svg"
-            variant="btn_white_text" 
-          />
-        </div>  
-         */}
+      <!-- component -->
+<div class="flex h-screen items-center justify-center bg-gray-50">
+  <div class="w-full md:w-1/2 rounded-lg bg-white px-8 py-4 shadow-md m-4">
+    <div class="px-1 py-4">
+      <h3 class="font-bold text-2xl font-sans">Share with others</h3>
+    </div>
+      <ul class="grid grid-cols-4 gap-2 px-1">
+        <li class="flex items-center flex-col">
+          <img src="https://randomuser.me/api/portraits/men/20.jpg" alt="" class="rounded-full w-16 h-16 object-cover">
+          <h5 class="font-semibold">Alex</h5>
+        </li>
+          <li class="flex items-center flex-col">
+          <img src="https://randomuser.me/api/portraits/women/11.jpg" alt="" class="rounded-full w-16 h-16 object-cover">
+          <h5 class="font-semibold">Sarah</h5>
+        </li>
+          <li class="flex items-center flex-col">
+          <img src="https://randomuser.me/api/portraits/men/32.jpg" alt="" class="rounded-full w-16 h-16 object-cover">
+          <h5 class="font-semibold">Jericho</h5>
+        </li>
+         </li>
+          <li class="flex items-center flex-col">
+          <img src="https://randomuser.me/api/portraits/women/28.jpg" alt="" class="rounded-full w-16 h-16 object-cover">
+          <h5 class="font-semibold">Dianna</h5>
+        </li>
+         <li class="flex items-center flex-col">
+          <img src="https://randomuser.me/api/portraits/men/66.jpg" alt="" class="rounded-full w-16 h-16 object-cover">
+          <h5 class="font-semibol">Bernard</h5>
+        </li>
+          <li class="flex items-center flex-col">
+          <img src="https://randomuser.me/api/portraits/women/19.jpg" alt="" class="rounded-full w-16 h-16 object-cover">
+          <h5 class="font-semibold">Francis</h5>
+        </li>
+          <li class="flex items-center flex-col">
+          <img src="https://randomuser.me/api/portraits/men/15.jpg" alt="" class="rounded-full w-16 h-16 object-cover">
+          <h5 class="font-semibold">Edna</h5>
+        </li>
+         </li>
+          <li class="flex items-center flex-col">
+          <img src="https://randomuser.me/api/portraits/men/21.jpg" alt="" class="rounded-full w-16 h-16 object-cover">
+          <h5 class="font-semibold">Carson</h5>
+        </li>
+      </ul>
+  </div>
+</div>
       </div>
-
-      <div className="relative flex flex-1 items-start">
-        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
-
-           <div className="flex flex-col">
-            <div className="flexBetween">
-              <p className="regular-16 text-gray-20">CRUSH Model</p>
-            </div>
-            <p className="bold-20 text-white">Ashley Zale</p>
-          </div>
-          <div className="flexBetween">
-            <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">Age</p>
-              <p className="bold-20 text-white">24</p>
-            </div>
-            <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">Personality</p>
-              <p className="bold-20 text-white">Flirty</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
     </section>
   )
 }
